bc31eb303d7be99f4773b0a5ea1666c5068220a1
Eugene Hong <awidesky@naver.com>
2021-07-21 17:38:39 +0900

    change code to print result

--- fsqrt/code/speed_test.cpp
+++ fsqrt/code/speed_test.cpp
@@ -2,6 +2,21 @@
 #include <iostream>
 #include <chrono>
 
+#define TESTS 10
+
+/*
+in desktop with i5-4590S 3.00GHz, 4GB
+
+mingw-w64 with x86_64-8.1.0-win32-seh-rt_v6-rev
+mysqrt avg : 4667.81ms
+stdlib avg : 10645.1ms
+2.28times faster
+
+MinGW-W64 x86_64-posix-seh, built by Brecht Sanders) 11.1.0
+mysqrt avg : 4651.74ms
+stdlib avg : 5255.95ms
+1.13 tiems faster
+*/
 float fsqrt_(float f) {
 
 	int i = *(int *)&f;
@@ -12,7 +27,7 @@ float fsqrt_(float f) {
 }
 
 
-void my_test() { 
+double my_test() { 
 
 	auto start = std::chrono::high_resolution_clock::now();
 
@@ -26,12 +41,13 @@ void my_test() {
 	}
 
 	auto end1 = std::chrono::high_resolution_clock::now();
-	auto diff = end1 - start;
-	std::cout << "mysqrt : " << std::chrono::duration <double, std::nano> (diff).count() << " ms" << std::endl;
+
+	std::cout << "mysqrt max : " << max << std::endl;
+	return std::chrono::duration<double, std::milli>(end1 - start).count();
 	
 }
 
-void lib_test() {
+double lib_test() {
 
 	auto start = std::chrono::high_resolution_clock::now();	
 
@@ -45,29 +61,33 @@ void lib_test() {
 	}
 
 	auto end1 = std::chrono::high_resolution_clock::now();
-	auto diff = end1 - start;
-	std::cout << "stdlib : " << std::chrono::duration <double, std::nano> (diff).count() << " ms" << std::endl;
+
+	std::cout << "stdlib max : " << max << std::endl;
+	return std::chrono::duration<double, std::milli>(end1 - start).count();
 	
 }
 
 
 int main() {
     
-	std::cout << "warmup" << std::endl;
+	std::cout << "* warmup" << std::endl;
 	for(int i = 0 ; i < 3 ; i++) {
 
 		my_test();
-		lib_test();
+		lib_test(); 
+	}
+
+	double my = 0.0, lib = 0.0;
+
+	std::cout << "\n\n* test\n" << std::endl;
+	for(int i = 0 ; i < TESTS ; i++) {
+
+		my += my_test();
+		lib += lib_test();
 
 	}
 
-
-	std::cout << "\n\ntest\n" << std::endl;
-	for(int i = 0 ; i < 5 ; i++) {
-
-		my_test();
-		lib_test();
-
-	}
+	std::cout << "\n\nmysqrt avg : " << (my / TESTS) << "ms\nstdlib avg : " << (lib / TESTS) << "ms" << "\n";
+	std::cout << "my sqrt is " << (lib/my) << " times faster than standard library" <<std::endl;
 
 }
